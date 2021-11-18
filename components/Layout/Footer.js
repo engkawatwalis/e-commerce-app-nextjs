8f22b942e8dd1ec92bafaea29a4adf05193146bb
engkawatwalis <engkawat.walis@gmail.com>
2021-11-18 17:32:52 +1100

    fixed small bug

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
                <li><Link href='/earphones'>earphones</Link></li>
            </ul>
             <p>
-            Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
+            {`Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.`}
             </p>
             <p>
             Copyright 2021. All Rights Reserved
